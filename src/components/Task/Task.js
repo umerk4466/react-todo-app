f7bf13760ba34f761fbc690a501756b42278f7f9
umerk4466 <umerk4466@gmail.com>
2020-11-12 21:18:46 +0000

    Updated

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -2,8 +2,15 @@ import './Task.css';
 
 function Task(props) {
   return (
-    <div>
-      <p>task</p>
+    <div className="TaskContainer">
+      <div className="TaskFlexContainer">
+        <div>First stediv of the tasks This  of the tasks This  of the tasks This   of the tasks This  of the tasks This   of the tasks This   of the tasks This  of the tasks This  of the tasks This   of the tasks This   of the tasks This   of the tasks This   of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This is a toast on a gam secondary background — check it out!.....</div>
+        <div><button className="btn ml-2"><i className="fa fa-ellipsis-v fa-2x"></i></button></div>
+      </div>
+      <div className="TaskFlexContainer mt-2">
+        <div className="TaskDate">Dues Date : 10-12-2020</div>
+        <span class="WorkingOn">Working on</span>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import AddTaskButton from './components/AddTaskButton/AddTaskButton';
 import Task from './components/Task/Task';
@@ -7,8 +9,12 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AddTaskButton/>
-    <Task/>
+    <div className="container-fluid">
+      <AddTaskButton/>
+      <Task/>
+      <Task/>
+    </div>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
