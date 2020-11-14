7c0a2a95f7e5ec2d3ca61f5c3281008162627459
umerk4466 <umerk4466@gmail.com>
2020-11-14 18:21:45 +0000

    Updated

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,61 +1,24 @@
-// import React from 'react';
-// import { motion, AnimatePresence } from "framer-motion"
-// import './Task.css';
-
-// function Task(props) {
-
-//   const animationVarient = {
-//     initial:{
-//       opacity:0
-//     },
-//     animate:{
-//       opacity:1,
-//       transition:{
-//         duration:3
-//       }
-//     },
-//   }
-//   let [expend, setExpend] = React.useState(false);
-//   let workingOn = props.workingOn ? <span className="WorkingOn">Working on</span> : null;
-//   let taskText =  expend ? <motion.div variants={animationVarient} initial="initial" animate="animate">{props.taskText}</motion.div> : <div>{props.taskText.substring(0,200)+"..."}</div>;
-  
-//   const toggleTask = () => {
-//     setExpend(!expend);
-//   }
-
-//   return (
-//     <div onClick={toggleTask} className="TaskContainer">
-//       <div className="TaskFlexContainer">
-//         {taskText}
-//         <div><button onClick={()=> alert("sk")} className="btn ml-2"><i className="fa fa-ellipsis-v fa-2x"></i></button></div>
-//       </div>
-//       <div className="TaskFlexContainer mt-2">
-//         <div className="TaskDate">Dues Date : 10-12-2020</div>
-//         {workingOn}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Task;
-
-
-
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import "./Task.css";
-
-
-
+import Bottleneck from "../Bottleneck/Bottleneck"
 
 
 function Task(props) {
   const [isOpen, setIsOpen] = React.useState(false);
   const toggleOpen = () => setIsOpen(!isOpen);
+  const workingOn = props.workingOn ? <span className="WorkingOn">Working on</span> : null;
 
   return (
-    <motion.li layout onClick={toggleOpen} initial={{x:30+"px"}} animate={{x:0}}>
-      <motion.div layout>{props.taskText}</motion.div>
+    <motion.li layout className="LiTaskContainer" onClick={toggleOpen} initial={{x:30+"px"}} animate={{x:0}}>
+      <motion.div layout className="TaskFlexContainer">
+        {props.taskText}
+        <div><button onClick={()=> alert("sk")} className="btn ml-2"><i className="fa fa-ellipsis-v fa-2x"></i></button></div>
+      </motion.div>
+      <motion.div layout className="TaskFlexContainer mt-2">
+        <div className="TaskDate">Dues Date : 10-12-2020</div>
+        {workingOn}
+      </motion.div>
       <AnimatePresence key={props.id}>{isOpen && <Content props={props} />}</AnimatePresence>
     </motion.li>
   );
@@ -73,9 +36,11 @@ function Content({props}) {
       exit={{ opacity: 0}}
       transition={{duration:0.5}}
     >
-      <div className="row" />
-      <div className="row" />
-      <div className="row" />
+      <Bottleneck/>
+      <Bottleneck/>
+      <Bottleneck/>
+
+      
     </motion.div>
   );
 }
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,51 +1,3 @@
-// import React from 'react';
-// import { motion, AnimatePresence } from "framer-motion"
-// import './Task.css';
-
-// function Task(props) {
-
-//   const animationVarient = {
-//     initial:{
-//       opacity:0
-//     },
-//     animate:{
-//       opacity:1,
-//       transition:{
-//         duration:3
-//       }
-//     },
-//   }
-//   let [expend, setExpend] = React.useState(false);
-//   let workingOn = props.workingOn ? <span className="WorkingOn">Working on</span> : null;
-//   let taskText =  expend ? <motion.div variants={animationVarient} initial="initial" animate="animate">{props.taskText}</motion.div> : <div>{props.taskText.substring(0,200)+"..."}</div>;
-  
-//   const toggleTask = () => {
-//     setExpend(!expend);
-//   }
-
-//   return (
-//     <div onClick={toggleTask} className="TaskContainer">
-//       <div className="TaskFlexContainer">
-//         {taskText}
-//         <div><button onClick={()=> alert("sk")} className="btn ml-2"><i className="fa fa-ellipsis-v fa-2x"></i></button></div>
-//       </div>
-//       <div className="TaskFlexContainer mt-2">
-//         <div className="TaskDate">Dues Date : 10-12-2020</div>
-//         {workingOn}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Task;
-
-
-
-
-
-
-
-
 import React from "react";
 import Task from '../Task/Task';
 import { motion, AnimateSharedLayout} from "framer-motion";
@@ -57,9 +9,9 @@ const tasksList = [0];
 function TaskList() {
   return (
     <AnimateSharedLayout>
-      <motion.ul layout>
+      <motion.ul className="UlTaskContainer">
         {tasksList.map((task) => (
-              <Task key={task} workingOn="true" taskText="First stediv of the tasks This  of the tasks This  of the tasks This   of the tasks This  of the tasks This   of the tasks This   of the tasks This  of the tasks This  of the tasks This   of the tasks This   of the tasks This   of the tasks This   of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This is a toast on a gam secondary background — check it out!." />
+          <Task key={task} workingOn="true" taskText="First stediv of the tasks This  of the tasks This  of the tasks This   of the tasks This  of the tasks This   of the tasks This   of the tasks This  of the tasks This  of the tasks This   of the tasks This   of the tasks This   of the tasks This   of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This  of the tasks This is a toast on a gam secondary background — check it out!." />
         ))}
       </motion.ul>
     </AnimateSharedLayout>
