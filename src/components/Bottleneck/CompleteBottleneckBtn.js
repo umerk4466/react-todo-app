131825687982fc731c363934bce44cfe0005ee91
umerk4466 <umerk4466@gmail.com>
2020-11-15 17:52:29 +0000

    Updated

--- src/components/Bottleneck/CompleteBottleneckBtn.js
+++ src/components/Bottleneck/CompleteBottleneckBtn.js
@@ -10,11 +10,9 @@ function CompleteBottleneckBtn({isCompleted}) {
     const btnFunction = isCompleted ? unFinishBottleneck : finishBottleneck;
     const btnClass = isCompleted ? "fa fa-undo" : "fa fa-check";
     return (
-        <div>
             <button onClick={btnFunction} className="btn ml-2">
                 <i className={btnClass}></i>
             </button>
-        </div>
     );
   }
   
